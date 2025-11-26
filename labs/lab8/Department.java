b0cb21189b755668721c382af20d1a07cd5bb105
Taaruni Ananya <106027676+tanya-ananya@users.noreply.github.com>
2025-11-25 19:34:53 -0500

    Add files via upload

---
+++ labs/lab8/Department.java
@@ -0,0 +1,26 @@
+public class Department {
+    private String dept_id;
+    private String dept_name;
+    private String location;
+
+    public String getDept_id() {
+        return dept_id;
+    }
+    public void setDept_id(String dept_id) {
+        this.dept_id = dept_id;
+    }
+
+    public String getDept_name() {
+        return dept_name;
+    }
+    public void setDept_name(String dept_name) {
+        this.dept_name = dept_name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
---
+++ labs/lab8/Enrollments.java
@@ -0,0 +1,29 @@
+public class Enrollments {
+
+	private int course_id, course_hours;
+	private String dept_id, course_name;
+	
+	public Enrollments() {
+		
+	}
+	
+	public void addCourse(String dept_id, int course_id, String course_name, int course_hours) {
+		this.dept_id = dept_id;
+		this.course_id = course_id;
+		this.course_name = course_name;
+		this.course_hours = course_hours;
+	}
+	public int getCourseID() {
+		return  course_id;
+	}
+	public int getCourseHours() {
+		return course_hours;
+	}
+	public String getDeptID() {
+		return dept_id;
+	}
+	public String getCourseName() {
+		return course_name;
+	}
+	
+}
